494d4e2b5356a9a3351283a67c1f1451bcf78910
FarisPalayi <farispalayi@gmail.com>
2021-10-06 15:01:24 +0530

    ♻ refactor: deep nested BEM class names

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,22 @@ function createRipple(event) {
   var button = event.currentTarget;
   var circle = document.createElement("span");
   var diameter = Math.max(button.clientWidth, button.clientHeight);
+  var height = button.clientHeight;
   var radius = diameter / 2;
 
   circle.style.width = circle.style.height = diameter + "px";
   circle.style.left = event.clientX - (button.offsetLeft + radius) + "px";
-  circle.style.top = event.clientY - (button.offsetTop + radius) + "px";
+  // circle.style.top = event.clientY - (button.offsetTop + radius) + "px";
+  circle.style.top = event.clientY;
+  console.log(event.clientY, button.offsetTop);
   circle.classList.add("ripple");
 
   button.appendChild(circle);
+
   circle.addEventListener("animationend", function () {
     button.removeChild(circle);
   });
 }
 
-var paymentBtn = document.getElementsByClassName("payment-btn")[0];
+var paymentBtn = document.getElementsByClassName("card__payment-btn")[0];
 paymentBtn.addEventListener("click", createRipple);
