ca26f0783e3842d963f7135f3b8d2779e7b3b122
FarisPalayi <farispalayi@gmail.com>
2021-10-10 19:16:49 +0530

    🐞 fix: ripple effect position

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+// Hero Image stuff
+
 var heroImgElm = document.querySelector(".card__img > div");
 
 var imgSource = "../images/illustration-hero.svg";
@@ -13,18 +15,17 @@ heroImg.addEventListener("load", function () {
   showHeroImg(heroImgElm, imgSource);
 });
 
+// Ripple stuff
+
 function createRipple(event) {
   var button = event.currentTarget;
   var circle = document.createElement("span");
   var diameter = Math.max(button.clientWidth, button.clientHeight);
-  var height = button.clientHeight;
   var radius = diameter / 2;
 
   circle.style.width = circle.style.height = diameter + "px";
   circle.style.left = event.clientX - (button.offsetLeft + radius) + "px";
-  // circle.style.top = event.clientY - (button.offsetTop + radius) + "px";
-  circle.style.top = event.clientY;
-  console.log(event.clientY, button.offsetTop);
+  circle.style.top = event.clientY - (button.offsetTop + radius) + "px";
   circle.classList.add("ripple");
 
   button.appendChild(circle);
